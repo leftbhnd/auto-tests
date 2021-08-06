e5932074f9aae1347975dbf22d135a5dd590fb29
d.laletin <d.laletin@promo-bot.ru>
2021-08-06 17:31:07 +0500

    fix: исправление ошибок после проверки методов

--- src/helpers/classes.py
+++ src/helpers/classes.py
@@ -11,11 +11,12 @@ class ClickMsg:
         self.z = z
 
 
-class FaseMsg:
-    def __init__(self, type, id, facemask, score):
+class FaceMsg:
+    def __init__(self, type, is_tracking, id, track_id, score):
         self.type = type
+        self.is_tracking = is_tracking
         self.id = id
-        self.facemask = facemask
+        self.track_id = track_id
         self.score = score
 
 
@@ -39,48 +40,120 @@ class JoyCmdMsg:
 
     def upVolume(self):
         # X LB
-        self._buttons[3] = 1
-        self._buttons[6] = 1
-        return {'axes': self._axes, 'buttons': self._buttons}
+        first_press_axes, first_press_buttons = list(
+            self._axes), list(self._buttons)
+        first_press_buttons[3], first_press_buttons[6] = 1, 1
+
+        second_press_axes, second_press_buttons = list(
+            self._axes), list(self._buttons)
+        second_press_buttons[3], second_press_buttons[6] = 1, 0
+
+        return [{'axes': first_press_axes, 'buttons': first_press_buttons},
+                {'axes': second_press_axes, 'buttons': second_press_buttons},
+                {'axes': self._axes, 'buttons': self._buttons}
+                ]
 
     def downVolume(self):
         # X LT
-        self._buttons[3] = 1
-        self._axes[5] = -1.0
-        return {'axes': self._axes, 'buttons': self._buttons}
+        first_press_axes, first_press_buttons = list(
+            self._axes), list(self._buttons)
+        first_press_buttons[3], first_press_axes[5] = 1, -1.0
+
+        second_press_axes, second_press_buttons = list(
+            self._axes), list(self._buttons)
+        second_press_buttons[3], second_press_axes[5] = 1, 1.0
+
+        return [{'axes': first_press_axes, 'buttons': first_press_buttons},
+                {'axes': second_press_axes, 'buttons': second_press_buttons},
+                {'axes': self._axes, 'buttons': self._buttons}
+                ]
 
     def upMic(self):
         # X RB
-        self._buttons[3] = 1
-        self._buttons[7] = 1
-        return {'axes': self._axes, 'buttons': self._buttons}
+        first_press_axes, first_press_buttons = list(
+            self._axes), list(self._buttons)
+        first_press_buttons[3], first_press_buttons[7] = 1, 1
+
+        second_press_axes, second_press_buttons = list(
+            self._axes), list(self._buttons)
+        second_press_buttons[3], second_press_buttons[7] = 1, 0
+
+        return [{'axes': first_press_axes, 'buttons': first_press_buttons},
+                {'axes': second_press_axes, 'buttons': second_press_buttons},
+                {'axes': self._axes, 'buttons': self._buttons}
+                ]
 
     def downMic(self):
         # X RT
-        self._buttons[3] = 1
-        self._axes[4] = -1.0
-        return {'axes': self._axes, 'buttons': self._buttons}
+        first_press_axes, first_press_buttons = list(
+            self._axes), list(self._buttons)
+        first_press_buttons[3], first_press_axes[4] = 1, -1.0
+
+        second_press_axes, second_press_buttons = list(
+            self._axes), list(self._buttons)
+        second_press_buttons[3], second_press_axes[4] = 1, 1.0
+
+        return [{'axes': first_press_axes, 'buttons': first_press_buttons},
+                {'axes': second_press_axes, 'buttons': second_press_buttons},
+                {'axes': self._axes, 'buttons': self._buttons}
+                ]
 
     def phraseMode(self):
         # back Y
-        self._buttons[15] = 1
-        self._buttons[4] = 1
-        return {'axes': self._axes, 'buttons': self._buttons}
+        first_press_axes, first_press_buttons = list(
+            self._axes), list(self._buttons)
+        first_press_buttons[15], first_press_buttons[4] = 1, 1
+
+        second_press_axes, second_press_buttons = list(
+            self._axes), list(self._buttons)
+        second_press_buttons[15], second_press_buttons[4] = 1, 0
+
+        return [{'axes': first_press_axes, 'buttons': first_press_buttons},
+                {'axes': second_press_axes, 'buttons': second_press_buttons},
+                {'axes': self._axes, 'buttons': self._buttons}
+                ]
 
     def nextPhrase(self):
         # Y RT
-        self._buttons[4] = 1
-        self._axes[4] = -1.0
-        return {'axes': self._axes, 'buttons': self._buttons}
+        first_press_axes, first_press_buttons = list(
+            self._axes), list(self._buttons)
+        first_press_buttons[4], first_press_axes[4] = 1, -1.0
+
+        second_press_axes, second_press_buttons = list(
+            self._axes), list(self._buttons)
+        second_press_buttons[4], second_press_axes[4] = 1, 1.0
+
+        return [{'axes': first_press_axes, 'buttons': first_press_buttons},
+                {'axes': second_press_axes, 'buttons': second_press_buttons},
+                {'axes': self._axes, 'buttons': self._buttons}
+                ]
 
     def previosePhrase(self):
         # Y RB
-        self._buttons[4] = 1
-        self._buttons[7] = 1
-        return {'axes': self._axes, 'buttons': self._buttons}
+        first_press_axes, first_press_buttons = list(
+            self._axes), list(self._buttons)
+        first_press_buttons[4], first_press_buttons[7] = 1, 1
+
+        second_press_axes, second_press_buttons = list(
+            self._axes), list(self._buttons)
+        second_press_buttons[4], second_press_buttons[7] = 1, 0
+
+        return [{'axes': first_press_axes, 'buttons': first_press_buttons},
+                {'axes': second_press_axes, 'buttons': second_press_buttons},
+                {'axes': self._axes, 'buttons': self._buttons}
+                ]
 
     def autoMode(self):
         # back start
-        self._buttons[15] = 1
-        self._buttons[11] = 1
-        return {'axes': self._axes, 'buttons': self._buttons}
+        first_press_axes, first_press_buttons = list(
+            self._axes), list(self._buttons)
+        first_press_buttons[15], first_press_buttons[11] = 1, 1
+
+        second_press_axes, second_press_buttons = list(
+            self._axes), list(self._buttons)
+        second_press_buttons[15], second_press_buttons[11] = 1, 0
+
+        return [{'axes': first_press_axes, 'buttons': first_press_buttons},
+                {'axes': second_press_axes, 'buttons': second_press_buttons},
+                {'axes': self._axes, 'buttons': self._buttons}
+                ]
