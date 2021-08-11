c45b21374c81c1d625a0251d5bbdf1057a56af5b
d.laletin <d.laletin@promo-bot.ru>
2021-08-11 16:05:40 +0500

    feat: добавлен тест проверки диалоговой строки

---
+++ src/tests/interface/проверка диалоговой строки/test_dialog_line.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+import time
+
+from src.helpers.messages import AsrTtsMsg
+'''
+63.33 seconds
+'''
+
+
+@pytest.mark.dialogLine
+def test_dialog_line(clickOn, node, screenDiffChecker):
+    clickOn('play')
+    clickOn('radius_modal_yes')
+    time.sleep(15)
+    node.cancelSpeechPub()
+    asr_msg = AsrTtsMsg('тестовое правило с лопатой',
+                        '1243c023-11a4-3344-bc21-0252ac131133'
+                        )
+    node.asrPub(asr_msg)
+    screenDiffChecker('dialog_line.png') is None
+
+
+@pytest.mark.dialogLine
+def test_restore(openPasswordModal, clickOn, typeText, screenDiffChecker):
+    openPasswordModal()
+    clickOn('pass_modal_input')
+    clickOn('choose_numbers')
+    typeText(['1', '2', '3', '4', '5', '6'])
+    clickOn('pass_modal_ok')
+    clickOn('restart')
+    clickOn('restart_modal_yes')
+    time.sleep(40)
+    assert screenDiffChecker('start.png') is None
