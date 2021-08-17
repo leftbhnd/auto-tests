9918a59cf02efac11e40149fd73e2ad4679ca393
d.laletin <d.laletin@promo-bot.ru>
2021-08-17 13:21:15 +0500

    refactor: объединение вспомогательной функции скриншота

--- src/helpers/screen_maker.py
+++ src/helpers/screen_maker.py
@@ -3,7 +3,25 @@
 
 import pyautogui
 
+
+def test(image, coordinates=(0, 40, 1280, 800)):
+    pyautogui.screenshot(
+        '/home/promobot/.tests/screens/%s.png' % (image), region=coordinates
+    )
+
+
+def getTuple(left, top, right, bottom):
+    return (left, top, right, bottom)
+
+
 screen_name = raw_input('название скриншота: ')
-pyautogui.screenshot(
-    '/home/promobot/.tests/screens/%s.png' % (screen_name), region=(0, 40, 1280, 800)
-)
+default = raw_input('задать координаты? 0 - пропустить (дефолт), 1 - задать: ')
+
+if default == '1':
+    left = raw_input('отступ слева: ')
+    top = raw_input('отступ сверху: ')
+    right = raw_input('отступ справа: ')
+    bottom = raw_input('отступ снизу: ')
+    test(screen_name, getTuple(left, top, right, bottom))
+else:
+    test(screen_name)
