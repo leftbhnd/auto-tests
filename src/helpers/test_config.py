d8833b9b99603350b7f445cfd7643b650fdfeeb8
d.laletin <d.laletin@promo-bot.ru>
2021-08-18 15:21:12 +0500

    test

--- src/helpers/test_config.py
+++ src/helpers/test_config.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-faster_timeout = 0.2
+faster_timeout = 0.1
 default_timeout = 0.5
 restart_timeout = 45
 running_timeout = 10
-modals_timeout = 5
+modals_timeout = 4
 screens_dir = '/home/promobot/.tests/screens/'
 failed_dir = '/home/promobot/.tests/screens/test_failed/'
