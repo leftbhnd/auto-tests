db8718a933da6afcbacb753399e4a49005dd3f1a
d.laletin <d.laletin@promo-bot.ru>
2021-08-18 11:40:37 +0500

    test: увеличил таймаут

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-faster_timeout = 0.1
-default_timeout = 0.4
+faster_timeout = 0.2
+default_timeout = 0.5
 screens_dir = '/home/promobot/.tests/screens/'
 failed_dir = '/home/promobot/.tests/screens/test_failed/'
 
