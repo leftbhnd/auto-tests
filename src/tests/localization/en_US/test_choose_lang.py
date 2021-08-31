4b78bc52fd4192132b8788cf31d8c6588b25f317
d.laletin <d.laletin@promo-bot.ru>
2021-08-31 16:57:56 +0500

    feat: тесты локализации на английском

---
+++ src/tests/localization/en_US/test_choose_lang.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+import time
+
+from src.helpers.config import modals, btn, modal
+'''
+21.17 seconds
+'''
+
+
+@pytest.mark.localization_en_US
+def test_choose_lang(clickOn, typeText, node):
+    node.initNode()
+    clickOn(btn.control)
+    clickOn(modal.pwd_input)
+    clickOn(btn.choose_numbers)
+    typeText('123456')
+    clickOn(modal.pwd_ok)
+    clickOn(btn.settings)
+    clickOn(btn.lang_settings)
+    clickOn(btn.lang_en_US)
+    clickOn(btn.lang_set_default)
+    clickOn(btn.back)
+    clickOn(modal.save_yes)
+    time.sleep(modals)
+    clickOn(btn.back)
+    clickOn(btn.back)
+    clickOn(btn.back)
+    time.sleep(modals)
+    node.killNode()
+    assert node.getSystemLanguage() == 'en_US'
