9f3ca860a6f5a430fd02ce20c91a06255be3370b
d.laletin <d.laletin@promo-bot.ru>
2021-09-06 15:39:44 +0500

    added coords

--- src/helpers/modal.py
+++ src/helpers/modal.py
@@ -71,6 +71,7 @@ class RobotModal(enum.Enum):
     ident_close_pt = (889, 290)
     ident_close_ro = (903, 293)
     ident_close_sv = (914, 289)
+    ident_close_tr = (892, 289)
     ident_kb_pwd_input = (675, 247)
     ident_kb_pwd_eye = (919, 248)
     ident_kb_confirm_input = (666, 287)
