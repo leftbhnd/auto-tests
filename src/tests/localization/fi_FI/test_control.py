8715f3fc274953ab75db830fabadd9e7c3122fc2
d.laletin <d.laletin@promo-bot.ru>
2021-09-07 12:19:24 +0500

    test

--- src/tests/localization/fi_FI/test_control.py
+++ src/tests/localization/fi_FI/test_control.py
@@ -41,7 +41,7 @@ def test_connection_open(clickOn, screenDiffChecker):
     time.sleep(slowly)
     assert screenDiffChecker(
         'localization/fi_FI/connection.png',
-        (0, 40, 920, 150)
+        (0, 40, 800, 150)
     ) is None
 
 
