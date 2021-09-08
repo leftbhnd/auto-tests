82fbdfd340553f8e75c5a005e2ef97515746cc6d
d.laletin <d.laletin@promo-bot.ru>
2021-09-08 16:31:22 +0500

    fix: добавлен возврат русского языка после прохождения тестов

--- src/tests/localization/zh_CN/test_settings_zh.py
+++ src/tests/localization/zh_CN/test_settings_zh.py
@@ -182,8 +182,27 @@ def test_update(clickOn, screenDiffChecker):
 
 
 @pytest.mark.localization_zh_CN
-def test_reset(clickOn):
+def test_reset(clickOn, typeText, node):
     clickOn(btn.back)
     clickOn(btn.back)
     clickOn(btn.back)
     time.sleep(modals)
+    clickOn(btn.control)
+    clickOn(modal.pwd_input)
+    clickOn(btn.choose_numbers)
+    typeText('123456')
+    clickOn(modal.pwd_ok)
+    clickOn(btn.settings)
+    clickOn(btn.lang_settings)
+    for i in range(12):
+        clickOn(btn.lang_down_arrow)
+    clickOn(btn.lang_ru_RU)
+    clickOn(btn.lang_set_default)
+    clickOn(btn.back)
+    clickOn(modal.save_yes)
+    time.sleep(modals)
+    clickOn(btn.back)
+    clickOn(btn.back)
+    clickOn(btn.back)
+    time.sleep(modals)
+    assert node.getSystemLanguage() == 'ru_RU'
