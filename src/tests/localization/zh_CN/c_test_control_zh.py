4e5f4136c2138dd52a7046d61cb5d0f7e9725763
d.laletin <d.laletin@promo-bot.ru>
2021-09-13 11:22:22 +0500

    style

--- src/tests/localization/zh_CN/c_test_control_zh.py
+++ src/tests/localization/zh_CN/c_test_control_zh.py
@@ -41,7 +41,7 @@ def test_connection_open(clickOn, screenDiffChecker):
     time.sleep(slowly)
     assert screenDiffChecker(
         'localization/zh_CN/con_connection.png',
-        (0, 40, 920, 150)
+        (0, 40, 900, 150)
     ) is None
 
 
