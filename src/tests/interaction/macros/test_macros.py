07b0026dbbd9ebe8bc0885763f07fa81c0c9224d
d.laletin <d.laletin@promo-bot.ru>
2021-09-20 16:55:56 +0500

    test

--- src/tests/interaction/macros/test_macros.py
+++ src/tests/interaction/macros/test_macros.py
@@ -7,7 +7,6 @@ from src.helpers.config import interaction
 from src.helpers.messages import InteractionMsg, AsrTtsMsg
 '''
 X seconds
-TODO проверить, что отвечает
 '''
 
 
@@ -23,7 +22,7 @@ def test_first_macros(node):
     node.cancelSpeechPub()
     asr_msg = AsrTtsMsg('макрос')
     node.asrPub(asr_msg)
-    assert node.getAnswer() == ''
+    assert node.getAnswer() == 'до повтора {repeat} после повтора'
 
 
 @pytest.mark.interaction_macros
@@ -31,7 +30,7 @@ def test_second_macros(node):
     node.cancelSpeechPub()
     asr_msg = AsrTtsMsg('макрос')
     node.asrPub(asr_msg)
-    assert node.getAnswer() == ''
+    assert node.getAnswer() == 'макрос'
 
 
 @pytest.mark.interaction_macros
@@ -39,7 +38,7 @@ def test_third_macros(node):
     node.cancelSpeechPub()
     asr_msg = AsrTtsMsg('макрос')
     node.asrPub(asr_msg)
-    assert node.getAnswer() == ''
+    assert node.getAnswer() == 'меня зовут {name} {pause:1} сегодня {date} я из города {city} владелец {user} компания {company} погода {weather} со мной говорили {visitors} на темы {themes} итнернет {internet} уровень заряда {battery} меня создали {dateofcreation} число перезагрузок {reboot_count} время работы {uptime}'
 
 
 @pytest.mark.interaction_macros
