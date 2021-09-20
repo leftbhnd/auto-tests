2157bd94514d91da7e4b4c7a53fc18361cdecef7
d.laletin <d.laletin@promo-bot.ru>
2021-09-20 18:01:17 +0500

    test

--- src/tests/interaction/macros/test_macros.py
+++ src/tests/interaction/macros/test_macros.py
@@ -40,6 +40,13 @@ def test_third_macros(node):
     node.asrPub(asr_msg)
     assert node.getAnswer() == 'меня зовут {name} {pause:1} сегодня {date} я из города {city} владелец {user} компания {company} погода {weather} со мной говорили {visitors} на темы {themes} итнернет {internet} уровень заряда {battery} меня создали {dateofcreation} число перезагрузок {reboot_count} время работы {uptime}'
 
+@pytest.mark.interaction_macros
+def test_fourth_macros(node):
+    node.cancelSpeechPub()
+    asr_msg = AsrTtsMsg('макрос')
+    node.asrPub(asr_msg)
+    assert node.getAnswer() == 'до повтора {repeat} после повтора {*1}'
+
 
 @pytest.mark.interaction_macros
 def test_restore(node):
