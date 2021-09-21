8869471dec6afb412c0ec04d8335ce45beb3dad3
d.laletin <d.laletin@promo-bot.ru>
2021-09-21 17:41:05 +0500

    test

--- src/services/faceRecognize.py
+++ src/services/faceRecognize.py
@@ -5,6 +5,7 @@ import rospy
 from promobot_msgs.msg import FaceArray
 from promobot_msgs.msg import Face
 from promobot_msgs.msg import FaceScore
+from src.helpers.messages import FaceMsg
 
 
 class FaceRecognizeService:
@@ -22,25 +23,51 @@ class FaceRecognizeService:
         face_array = FaceArray()
         face = Face()
         face_score = FaceScore()
-        face.type = data.type
+        face_msg = FaceMsg(
+            data.type, data.is_tracking,
+            data.id, data.track_id, data.score
+        )
+        # имитируем нераспознанное лицо
+        face.type = 1
         face.source = 1
-        face.is_tracking = data.is_tracking
-        face.track_id = data.track_id
-        face.id = data.id
+        face.is_tracking = True
+        face.track_id = 0
+        face.id = -1
         face.gender = 0
         face.age = 0.0
         face.emotion = ''
         face.liveness_type = 0
-        face_score.source = 3
-        face_score.personSource = 1
-        face_score.id = data.id
-        face_score.score = data.score
-        face.persons = [face_score]
         # чистим массив лиц перед отправкой
         face_array.faces = []
         self._pub_face_to_faceArray.publish(face_array)
         rospy.sleep(self._timeout)
         # публикуем нужное лицо
+        face.type = face_msg.type
+        face.is_tracking = face_msg.is_tracking
+        face.track_id = face_msg.track_id
+        face.id = face_msg.id
+        face_score.source = 2
+        face_score.personSource = 1
+        face_score.id = data.id
+        face_score.score = data.score
+        face.persons = [face_score]
         face_array.faces.append(face)
+        self._pub_face_to_faceArray.publish(face_array)
+        rospy.sleep(self._timeout)
+
+    def clearFacePub(self):
+        face_array = FaceArray()
+        face = Face()
+        # имитируем нераспознанное лицо
+        face.type = 0
+        face.source = 1
+        face.is_tracking = False
+        face.track_id = 0
+        face.id = -2
+        face.gender = 0
+        face.age = 0.0
+        face.emotion = ''
+        face.liveness_type = 0
+        face_array.faces = []
         self._pub_face_to_faceArray.publish(face_array)
         rospy.sleep(self._timeout)
