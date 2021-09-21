a86acf840117d89ed6607935b0ec7e99c906b725
d.laletin <d.laletin@promo-bot.ru>
2021-09-21 13:12:03 +0500

    test

--- src/tests/interaction/levels/test_dnd.py
+++ src/tests/interaction/levels/test_dnd.py
@@ -4,12 +4,9 @@ from src.conftest import clickOn
 import pytest
 import time
 
-from src.helpers.config import btn
 from src.helpers.messages import SwipeMsg
 
 
-def test_test(clickOn, dNd):
-    clickOn(btn.lingvo_first_level)
-    time.sleep(5)
+def test_test(dNd):
     swipe_msg = SwipeMsg((446, 233), (446, 655))
     dNd(swipe_msg)
