c134b96610db4336ad991c222ff660d29303851a
d.laletin <d.laletin@promo-bot.ru>
2021-09-21 10:58:43 +0500

    test

--- src/tests/interaction/promo/test_promo.py
+++ src/tests/interaction/promo/test_promo.py
@@ -10,7 +10,30 @@ X seconds
 
 
 @pytest.mark.interaction_promo
-def test_first_pictures(screenDiffChecker):
+def test_add_promo(clickOn, typeText, openPwdModal, screenDiffChecker):
+    openPwdModal()
+    clickOn(modal.pwd_input)
+    clickOn(btn.choose_numbers)
+    typeText('123456')
+    clickOn(modal.pwd_ok)
+    clickOn(btn.promo)
+    clickOn(btn.promo_pictures)
+    clickOn(btn.promo_pictures)
+    clickOn(btn.promo_fs_checkbox1)
+    clickOn(btn.promo_fs_checkbox2)
+    clickOn(btn.promo_add)
+    clickOn(modal.promo_yes)
+    assert screenDiffChecker(
+        'interaction/promo_pictures.png'
+    ) is None
+
+
+@pytest.mark.interaction_promo
+def test_first_pictures(clickOn, screenDiffChecker):
+    clickOn(btn.back)
+    clickOn(modal.save_yes)
+    clickOn(btn.back)
+    time.sleep(modals)
     assert screenDiffChecker(
         'interaction/promo1.png',
         (0, 40, 1280, 660)
@@ -30,6 +53,7 @@ def test_second_picture(screenDiffChecker):
 def test_delete_pictures(clickOn, openPwdModal, typeText, screenDiffChecker):
     openPwdModal()
     clickOn(modal.pwd_input)
+    clickOn(btn.choose_numbers)
     typeText('123456')
     clickOn(modal.pwd_ok)
     clickOn(btn.promo)
