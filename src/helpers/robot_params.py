4a1d14db4ce3d7a4ee5ecbd37ddff587fc0d0f35
d.laletin <d.laletin@promo-bot.ru>
2021-09-23 17:23:11 +0500

    test

--- src/helpers/robot_params.py
+++ src/helpers/robot_params.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#from src.helpers.params.dialog_params import DialogParams
+from src.helpers.params.dialog_params import DialogParams
 from src.helpers.params.driving_params import DrivingParams
 from src.helpers.params.interaction_params import InteractionParams
 
@@ -11,7 +11,5 @@ class RobotParams:
         self.driving = DrivingParams
         self.interaction = InteractionParams
 
-
-DialogParams = 9
 
 robot_params = RobotParams(DialogParams, DrivingParams, InteractionParams)
