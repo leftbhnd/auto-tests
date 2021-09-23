b17df53845442d4d96e7421fa8d6b017b5877ea9
d.laletin <d.laletin@promo-bot.ru>
2021-09-23 17:44:49 +0500

    fix: добавлен init файл

---
+++ src/helpers/params/interaction_params.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import enum
+
+
+class InteractionParams(enum.Enum):
+    startBySpeech = (1119, 201)
+    startBySpeechEnable = (893, 253)
+    startBySpeechDisable = (914, 146)
+    updateBySpeech = (1119, 453)
+    updateBySpeechEnable = (900, 501)
+    updateBySpeechDisable = (894, 400)
+    startByFace = (1117, 389)
+    startByFaceEnable = (919, 447)
+    startByFaceDisable = (907, 352)
+    updateByFace = (1123, 529)
+    updateByFaceEnable = (936, 583)
+    updateByFaceDisable = (922, 485)
---
+++ src/helpers/robot_params.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from src.helpers.params.dialog_params import DialogParams
+from src.helpers.params.driving_params import DrivingParams
+from src.helpers.params.interaction_params import InteractionParams
+
+
+class RobotParams:
+    def __init__(self, DialogParams, DrivingParams, InteractionParams):
+        self.dialog = DialogParams
+        self.driving = DrivingParams
+        self.interaction = InteractionParams
+
+
+robot_parameters = RobotParams(
+    DialogParams, DrivingParams, InteractionParams
+)
