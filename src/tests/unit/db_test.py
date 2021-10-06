d854ef9d3e66d838d9ff67c0534d585227f2b297
d.laletin <d.laletin@promo-bot.ru>
2021-10-06 11:44:15 +0500

    test(unit): написаны тесты для работы с основными параметрами

--- src/tests/unit/db_test.py
+++ src/tests/unit/db_test.py
@@ -5,30 +5,108 @@ import pytest
 
 @pytest.mark.unit
 def test_get_default(db):
-    assert db.get(
-        'robot_settings/driving/radius'
-    ) == ['robot_settings/driving/radius', '5']
+    assert db.getValue('/driving/radius') == '5'
 
 
 @pytest.mark.unit
-def test_set(db):
-    assert db.update(3, 'robot_settings/driving/radius') == 1
+def test_update_value(db):
+    assert db.updateValue([{'name': '/driving/radius', 'value': 3}]) == 1
 
 
 @pytest.mark.unit
-def test_get_new(db):
-    assert db.get(
-        'robot_settings/driving/radius'
-    ) == ['robot_settings/driving/radius', '3']
+def test_get_new_value(db):
+    assert db.getValue('/driving/radius') == '3'
 
 
 @pytest.mark.unit
-def test_restore_default(db):
-    assert db.update(5, 'robot_settings/driving/radius') == 1
+def test_restore_default_value(db):
+    assert db.updateValue([{'name': '/driving/radius', 'value': 5}]) == 1
 
 
 @pytest.mark.unit
-def test_check_default(db):
-    assert db.get(
-        'robot_settings/driving/radius'
-    ) == ['robot_settings/driving/radius', '5']
+def test_check_default_value(db):
+    assert db.getValue('/driving/radius') == '5'
+
+
+@pytest.mark.unit
+def test_map_on(db):
+    assert db.updateValue([
+        {'name': '/driving/useMap', 'value': True},
+        {'name': '/driving/useRadius', 'value': False}
+    ]) == 2
+
+
+@pytest.mark.unit
+def test_map_get_value_use_map(db):
+    assert db.getValue('/driving/useMap') == 'true'
+
+
+@pytest.mark.unit
+def test_map_get_value_use_radius(db):
+    assert db.getValue('/driving/useRadius') == 'false'
+
+
+@pytest.mark.unit
+def test_radius_on(db):
+    assert db.updateValue([
+        {'name': '/driving/useMap', 'value': False},
+        {'name': '/driving/useRadius', 'value': True}
+    ]) == 2
+
+
+@pytest.mark.unit
+def test_radius_get_value_use_map(db):
+    assert db.getValue('/driving/useMap') == 'false'
+
+
+@pytest.mark.unit
+def test_radius_get_value_use_radius(db):
+    assert db.getValue('/driving/useRadius') == 'true'
+
+
+@pytest.mark.unit
+def test_bmp_on(db):
+    assert db.updateValue([
+        {'name': '/navigation/backMainPoint', 'value': True}
+    ]) == 1
+
+
+@pytest.mark.unit
+def test_bmp_on_get(db):
+    assert db.getValue('/navigation/backMainPoint') == 'true'
+
+
+@pytest.mark.unit
+def test_bmp_off(db):
+    assert db.updateValue([
+        {'name': '/navigation/backMainPoint', 'value': False}
+    ]) == 1
+
+
+@pytest.mark.unit
+def test_bmp_off_get(db):
+    assert db.getValue('/navigation/backMainPoint') == 'false'
+
+
+@pytest.mark.unit
+def test_ignoreInteraction_on(db):
+    assert db.updateValue([
+        {'name': '/navigation/ignoreInteractions', 'value': True}
+    ]) == 1
+
+
+@pytest.mark.unit
+def test_ignoreInteraction_on_get(db):
+    assert db.getValue('/navigation/ignoreInteractions') == 'true'
+
+
+@pytest.mark.unit
+def test_ignoreInteraction_off(db):
+    assert db.updateValue([
+        {'name': '/navigation/ignoreInteractions', 'value': False}
+    ]) == 1
+
+
+@pytest.mark.unit
+def test_ignoreInteraction_off_get(db):
+    assert db.getValue('/navigation/ignoreInteractions') == 'false'
