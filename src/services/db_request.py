e00721c7201fe44c1e02123bfcb55fad9c0dad29
d.laletin <d.laletin@promo-bot.ru>
2021-10-06 11:43:13 +0500

    refactor: обновлен класс для работы с БД робота

--- src/services/db_request.py
+++ src/services/db_request.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import psycopg2
 
-from db_access import login, password
+from db_access import db_name, login, password, update, select
 
 
 class DbRequest:
     def __init__(self):
         self._connection = psycopg2.connect(
-            dbname='promobot', user=login,
+            dbname=db_name, user=login,
             password=password
         )
         self._cursor = self._connection.cursor()
-        self._update = """update robotsettings
-            set value = %s
-            where parameter=%s"""
+        self._update = update
+        self._select = select
         self._updated_rows = 0
 
-    def update(self, value, param):
-        self._cursor.execute(
-            self._update, (value, param)
-        )
-        self._updated_rows = self._cursor.rowcount
-        self._connection.commit()
+    def _closeConnection(self):
         self._cursor.close()
         self._connection.close()
+
+    def _createParamName(self, name):
+        return 'robot_settings' + name
+
+    def updateValue(self, params):
+        for param in params:
+            self._cursor.execute(
+                self._update, (
+                    param['value'],
+                    self._createParamName(param['name'])
+                )
+            )
+            self._updated_rows += self._cursor.rowcount
+        self._connection.commit()
+        self._closeConnection()
         return self._updated_rows
 
-    def get(self, param):
+    def getValue(self, param):
         self._cursor.execute(
-            'select * from robotsettings where parameter=%s', (
-                param,
+            self._select, (
+                self._createParamName(param),
             )
         )
         for row in self._cursor:
-            return [row[2], row[3]]
-        self._cursor.close()
-        self._connection.close()
+            return row[3]
+        self._closeConnection()
