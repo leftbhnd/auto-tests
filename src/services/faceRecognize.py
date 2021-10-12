476662c4a06723dcbb2898d18548c24fc2ff2a7b
d.laletin <d.laletin@promo-bot.ru>
2021-10-12 12:43:14 +0500

    fix: удалена очистка массива лиц (для нее появился новый метод)

--- src/services/faceRecognize.py
+++ src/services/faceRecognize.py
@@ -22,24 +22,16 @@ class FaceRecognizeService:
         face_array = FaceArray()
         face = Face()
         face_score = FaceScore()
-        # имитируем появление нераспознанного лица
-        face.type = 1
-        face.source = 1
+        # публикуем нужное лицо
+        face.type = type
+        face.source = source
         face.is_tracking = True
-        face.track_id = 0
-        face.id = -1
+        face.track_id = track_id
+        face.id = id
         face.gender = 0
         face.age = 0.0
         face.emotion = ''
         face.liveness_type = 0
-        face.persons = []
-        face_array.faces.append(face)
-        self._pub_face_to_faceArray.publish(face_array)
-        # публикуем нужное лицо
-        face.type = type
-        face.is_tracking = True
-        face.track_id = track_id
-        face.id = id
         face_score.source = source
         face_score.personSource = 1
         face_score.id = id
