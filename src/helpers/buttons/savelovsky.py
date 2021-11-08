1535fcfa31c2f1e1ae453620f657f92b0df24e76
d.laletin <d.laletin@promo-bot.ru>
2021-11-08 12:32:55 +0500

    feat: добавлены координаты для кейса контрола, поправлен конфиг

---
+++ src/helpers/buttons/savelovsky.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import enum
+
+
+class Savelovsky(enum.Enum):
+    start = (951, 540)
+    name = (712, 247)
+    surname = (140, 247)
+    phone = (1283, 253)
+    confirm = (1223, 407)
+    # пока не знаю координаты, нужно восстановить экран
+    save = (1, 1)
+    free_enter = (873, 254)
+    choose = (946, 962)
+    get_check = (1261, 689)
+    kb_a = (712, 822)
+    kb_number = (1010, 921)
---
+++ src/helpers/savelovsky_buttons.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from src.helpers.buttons.savelovsky import Savelovsky
+
+
+class SavelovskyButtons:
+    def __init__(self, Savelovsky):
+        self.savelovsky = Savelovsky
+
+
+savelovsky_buttons = SavelovskyButtons(Savelovsky)
