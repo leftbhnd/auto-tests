ffd6994f98006f06fc574f5e61264601c529940b
d.laletin <d.laletin@promo-bot.ru>
2021-11-17 16:29:29 +0500

    fix: исправлена запуск окружения

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y scrot
 sudo pip install virtualenv==20.4.7
-virtualenv env
+virtualenv -p /usr/bin/python env
 source env/bin/activate
 pip install -r requirements.txt
